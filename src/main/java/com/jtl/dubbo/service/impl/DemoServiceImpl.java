5ddfa94e2d6ec2fa388fd071aab1895ec8ee013a
jiangtenglong <jiangtenglong@wjika.com>
2016-11-18 15:28:58 +0800

    修改服务端

--- src/main/java/com/jtl/dubbo/service/impl/DemoServiceImpl.java
+++ src/main/java/com/jtl/dubbo/service/impl/DemoServiceImpl.java
@@ -1,10 +1,12 @@
 package com.jtl.dubbo.service.impl;
 
 import com.jtl.dubbo.service.DemoService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Administrator on 2016/11/17 0017.
  */
+@Service("demoService")
 public class DemoServiceImpl implements DemoService {
     public String demoFun() {
         System.out.println("dubbo hello");
